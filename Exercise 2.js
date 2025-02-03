7d723f5fb0d33da1bb138dc30709cdd5074f2f6f
nuno2msilva <166436702+nuno2msilva@users.noreply.github.com>
2025-02-03 19:11:39 +0000

    deleted:    Exercise 1.js 	deleted:    Exercise 2.js 	deleted:    Exercise 3.js 	deleted:    Exercise 4.js 	Session 1/

--- Exercise 2.js
+++
@@ -1,9 +0,0 @@
-[Start] → [GET Desired Temperature FROM User] → [Measure Current Temperature]
-             ↓                                   ↑
-[Compare Current TO Desired] ←─────── [Wait FOR Interval]
-             │
-             ├─ IF Current < Desired → [Activate Heater] → [Wait]
-             │
-             ├─ IF Current > Desired → [Activate AC] → [Wait]
-             │  
-             └─ IF Current == Desired → [Do Nothing] → [Wait]
